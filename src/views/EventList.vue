eee3210a8887cfa3ee4fb06654a5a4fd6be16c60
Bot Madera <botmadera@gmail.com>
2021-01-12 17:37:19 -0300

    Commit message

---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="events">
+    <event-card 
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '../services/EventService'
+
+export default {
+  name: 'EventList',
+  components: {
+    'event-card':EventCard
+  },
+  data(){
+    return{
+      events: null
+    }
+  },
+  created(){
+    EventService.getEvents()
+      .then(response =>{
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      }) 
+  }
+}
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
